54bafdcc69c9cf526491f2b269791f4c58aed9ba
Navaneeth Nivol <nivol_1999@github.com>
2018-06-23 18:54:17 +0530

    Done!

--- tictactoe.c
+++ tictactoe.c
@@ -2,7 +2,7 @@
 #include<conio.h>
 #include<stdlib.h>
 
-char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char square[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
 int checkwin();
 void board();
